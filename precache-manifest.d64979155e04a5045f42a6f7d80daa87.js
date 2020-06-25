593ea8570eaacb11f03fca81000a3927d95ff9ac
mariacristinatovarsalas <58868315+mariacristinatovarsalas@users.noreply.github.com>
2020-06-25 01:41:53 -0300

    Updates

---
+++ precache-manifest.d64979155e04a5045f42a6f7d80daa87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be6bd2cd17c1ed1e5220d927f188539",
+    "url": "/things-to-do-app/index.html"
+  },
+  {
+    "revision": "056d078a4014d085b2c9",
+    "url": "/things-to-do-app/static/css/main.f94d1fec.chunk.css"
+  },
+  {
+    "revision": "d213e54e3fada6cc5630",
+    "url": "/things-to-do-app/static/js/2.f28f164a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/things-to-do-app/static/js/2.f28f164a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "056d078a4014d085b2c9",
+    "url": "/things-to-do-app/static/js/main.379b7d1e.chunk.js"
+  },
+  {
+    "revision": "1a989b913935030ec722",
+    "url": "/things-to-do-app/static/js/runtime-main.8ca14fd4.js"
+  }
+]);
